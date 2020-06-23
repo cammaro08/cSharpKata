d045ef92f6200af51864458ed39e2833dcc3dd83
TS-Personal <talha.i.sheikh@gmail.com>
2020-06-23 13:03:09 +0100

    hussein_free_youtube_tut

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace hussein_free_youtube
+﻿using System;
+
+namespace hussein_free_youtube
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.Write("Enter tweet to post: ");
+            string tweet = Console.ReadLine();
+            Console.Write("Your Tweet is: ");
+            Console.WriteLine(tweet);
+            Console.WriteLine("Your tweet length is: " + tweet.Length);
+            
         }
     }
 }
